e7eac93775950c36e0ec6ea3f777d934ffd1c92d
Vladislav Prekel <misterptits@yandex.ru>
2020-12-01 20:23:31 +0700

    t

---
+++ MyStore/MyStore.SQLProvider/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open FSharp.Data.Sql
+
+type sql =
+    SqlDataProvider<Common.DatabaseProviderTypes.POSTGRESQL, "Host=localhost;Database=postgres;Username=postgres;Password=qwerty123">
+
+let ctx = sql.GetDataContext()
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello world %A" ctx
+    0 // return an integer exit code
