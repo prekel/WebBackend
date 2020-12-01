53098d08d665b246daf0e84756bae31d2a3b8a06
Vladislav Prekel <misterptits@yandex.ru>
2020-12-01 21:57:48 +0700

    annas

--- MyStore/MyStore.SQLProvider/Program.fs
+++ MyStore/MyStore.SQLProvider/Program.fs
@@ -11,17 +11,39 @@ let connStr =
 
 type HR = SqlDataProvider<Common.DatabaseProviderTypes.POSTGRESQL, connStr, ResolutionPath=resolutionPath>
 
+
 [<EntryPoint>]
 let main argv =
     Console.OutputEncoding <- Encoding.UTF8
     let ctx = HR.GetDataContext()
 
+    FSharp.Data.Sql.Common.QueryEvents.SqlQueryEvent
+    |> Event.add (printfn "Executing SQL: %O")
+
     let employeesFirstName =
         query {
-            for emp in ctx.Public.Customers do
-                select emp.FirstName
+            for customer in ctx.Public.Customers do
+                where (customer.CustomerId = 34)
+                select $"{customer.FirstName} {customer.LastName}"
         }
-        |> Seq.head
 
-    printfn "Hello %s!" employeesFirstName
+    let cnt =
+        query {
+            for customer in ctx.Public.Customers do
+                where (customer.CustomerId = 34)
+                count
+        }
+
+    let annas =
+        query {
+            for customer in ctx.Public.Customers do
+                where (customer.FirstName = "Анна")
+                select customer
+        }
+
+    annas
+    |> Seq.toList
+    |> List.map (fun customer -> printf "%s\n" $"{customer.FirstName} {customer.LastName}")
+    |> ignore
+
     0
