014dc5e76df70dbcdc8c33bff6df58112db931d6
Vladislav Prekel <misterptits@yandex.ru>
2021-05-30 18:55:01 +0700

    router

--- MyStore.New/MyStore.MvcFSharp/Content/fsharp/Main.fs.js
+++
@@ -1,8 +0,0 @@
-import { render } from "react-dom";
-import { createElement } from "react";
-import { Components_Router } from "./Components.fs.js";
-import "./styles/global.scss";
-
-
-render(createElement(Components_Router, null), document.getElementById("feliz-app"));
-
--- MyStore.New/MyStore.MvcFSharp/Startup.fs
+++ MyStore.New/MyStore.MvcFSharp/Startup.fs
@@ -21,61 +21,9 @@ open Giraffe
 open Giraffe.EndpointRouting
 open Giraffe.Razor
 open MyStore.MvcFSharp.Models
+open MyStore.MvcFSharp.Router
 
 type Startup(configuration: IConfiguration) =
-    let handler1 : HttpHandler =
-        fun (_: HttpFunc) (ctx: HttpContext) -> ctx.WriteTextAsync "Hello World"
-
-    let handler2 (firstName: string, age: int) : HttpHandler =
-        fun (_: HttpFunc) (ctx: HttpContext) ->
-            sprintf "Hello %s, you are %i years old." firstName age
-            |> ctx.WriteTextAsync
-
-    let handler3 (a: string, b: string, c: string, d: int) : HttpHandler =
-        fun (_: HttpFunc) (ctx: HttpContext) ->
-            sprintf "Hello %s %s %s %i" a b c d
-            |> ctx.WriteTextAsync
-
-    let indexHandler =
-        razorHtmlView "Home/Index" None None None
-
-    let privacyHandler =
-        razorHtmlView "Home/Privacy" None None None
-
-    let errorHandler =
-        fun (next: HttpFunc) (ctx: HttpContext) ->
-            task {
-                let reqId =
-                    if isNull Activity.Current then
-                        ctx.TraceIdentifier
-                    else
-                        Activity.Current.Id
-
-                return! razorHtmlView "Home/Error" (Some { RequestId = reqId }) None None next ctx
-            }
-
-    let antiforgeryTokenHandler =
-        text "Bad antiforgery token"
-        |> RequestErrors.badRequest
-        |> validateAntiforgeryToken
-
-    let endpoints1 =
-        [ subRoute "/foo" [ GET [ route "/bar" (text "Aloha!") ] ]
-          GET [ route "/" (text "Hello World")
-                routef "/%s/%i" handler2
-                routef "/%s/%s/%s/%i" handler3 ]
-          GET_HEAD [ route "/foo" (text "Bar")
-                     route "/x" (text "y")
-                     route "/abc" (text "def") ]
-          // Not specifying a http verb means it will listen to all verbs
-          subRoute "/sub" [ route "/test" handler1 ]
-          subRoute
-              "/Home"
-              [ GET [ route "/" (indexHandler >=> antiforgeryTokenHandler)
-                      route "/Privacy" privacyHandler
-                      route "/Error" errorHandler ] ] ]
-
-
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
