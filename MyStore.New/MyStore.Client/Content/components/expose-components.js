5c20a00ca35c79e2f98658fd636f7c1c8396b813
Vladislav Prekel <misterptits@yandex.ru>
2021-05-31 10:07:57 +0700

    qwe

--- MyStore.New/MyStore.Client/Content/components/expose-components.js
+++ MyStore.New/MyStore.Client/Content/components/expose-components.js
@@ -4,8 +4,11 @@ import ReactDOMServer from 'react-dom/server';
 
 import RootComponent from './home.jsx';
 
+import Root1 from '../src/App.jsx'
+
 global.React = React;
 global.ReactDOM = ReactDOM;
 global.ReactDOMServer = ReactDOMServer;
 
 global.Components = { RootComponent };
+global.Components1 = { Root1 };
